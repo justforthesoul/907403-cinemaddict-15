64f826a92b71f0f0a25c75675e1ccd61c0b0c0c0
just_for_the_soul (Лига А) <just_for_the_soul@mail.ru>
2021-08-01 17:13:06 +0200

    поправил название переменной, а то watchlist очень напрягает

--- src/view/site-menu-template.js
+++ src/view/site-menu-template.js
@@ -1,8 +1,8 @@
-export const createSiteMenuTemplate = ({watchlist, history, favorites}) => {
+export const createSiteMenuTemplate = ({watchList, history, favorites}) => {
   return `<nav class="main-navigation">
             <div class="main-navigation__items">
               <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
-              <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">${watchlist}</span></a>
+              <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">${watchList}</span></a>
               <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">${history}</span></a>
               <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">${favorites}</span></a>
             </div>
